988673677f3f614b9e415c9d11248ae1283df980
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-02-10 13:13:26 +0000

    add task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+void bubble_sort(int *array, size_t size)
+{
+	size_t tmp, i, j;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (i = 1; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# 0x1B. C - Sorting algorithms & Big O
+### ![](https://s3.amazonaws.com/intranet-projects-files/holbertonschool-low_level_programming/248/willy-wonka.png)
+
+##
+```
+
+```
+
+## Mandatory 
+***
+## 
+## 
+### 
+```
+
+```
+
+
+## Contributors
+
+ ### [Axel Valentin](https://github.com/Pixeloceax)
+
+# for Holberton School
